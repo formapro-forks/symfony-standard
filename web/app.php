c672735f87019284bfb6afed50d41aeb6f6b54c6
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-03-06 16:51:16 +0200

    add sentry client lib and its env var

--- web/app.php
+++ web/app.php
@@ -2,14 +2,18 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__.'/../app/autoload.php';
+
+if ($sentryDsn = getenv('SENTRY_DSN')) {
+    new Raven_Client($sentryDsn);
+}
+
 if (false == $env = getenv('SYMFONY_ENV')) {
     throw new \LogicException('Tne SYMFONY_ENV env var is not set.');
 }
 
 $debug = (bool) getenv('SYMFONY_DEBUG');
-
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
 
 $kernel = new AppKernel($env, $debug);
 $request = Request::createFromGlobals();
