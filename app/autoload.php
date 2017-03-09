2a7245ba20d40304e2b2a78a61ac1b9b7aa9b78a
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-03-09 17:00:52 +0200

    fix for sensio framework extra.

--- app/autoload.php
+++ app/autoload.php
@@ -5,4 +5,6 @@ use Composer\Autoload\ClassLoader;
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
 return $loader;
